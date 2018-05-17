ef11cafb2ced27bc341ca639982504151d8365e0
Samiraj Panneer Selvam, Senthil <Senthil.SamirajPanneerSelvam@cdk.com>
2018-05-17 16:29:43 -0500

    Expose secondary and style of FAB as props in SpeedDialItem

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SpeedDialItem = exports.SpeedDial = undefined;
+
+var _speedDial = require('./speed-dial');
+
+var _speedDialItem = require('./speed-dial-item');
+
+exports.SpeedDial = _speedDial.SpeedDial;
+exports.SpeedDialItem = _speedDialItem.SpeedDialItem;
